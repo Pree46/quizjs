633f13e45d4fa6be1b1b3e350d9d3b1c4cbb1127
Pree46 <preethirajagopal464@gmail.com>
2023-10-10 18:49:42 +0530

    Functionality done

--- src/components/Questionbox.js
+++ src/components/Questionbox.js
@@ -1,23 +1,27 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-const QuestionBox = ({question, options}) => {
+const QuestionBox = ({ question, options, selected }) => { // Add 'selected' prop here
 
-    const [answers, setAnswer] = useState(options);
-    return(
-        <div className="questionBox">
-            <div className="question">{question}</div>
+  const [answers, setAnswer] = useState(options);
 
-            {answers.map((text, index) => (
-                <button key={index} className="answerBtn" onClick={()=>{
-                    setAnswer([text]);
-                    selected([text]);
-                }}>
-                    {text}
-                </button>
-            ))}
-        </div>
-    );
+  return (
+    <div className="questionBox">
+      <div className="question">{question}</div>
 
+      {answers.map((text, index) => (
+        <button
+          key={index}
+          className="answerBtn"
+          onClick={() => {
+            setAnswer([text]);
+            selected(text); // Pass the selected answer back to the parent component
+          }}
+        >
+          {text}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default QuestionBox;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import "./assets/style.css"; // Check the path to your CSS file
 import quizService from "./quizService/index.js"
 import QuestionBox from "./components/Questionbox.js";
+import Result from "./components/Result";
 
 class QuizJS extends Component {
     state = {
-        qBank: []
+        qBank: [],
+        score: 0,
+        responses: 0
     };
     getQuestions = () => {
         quizService().then( question => {
@@ -16,11 +19,26 @@ class QuizJS extends Component {
         });
     }
     computeAnswer = (answer, correctAnswer) => {
-        if (answer == correctAnswer) {
+        if (answer === correctAnswer) {
             this.setState({
                 score: this.state.score +1
-            })
+            });
         }
+    this.setState({
+
+        responses: this.state.responses < 5 ?  this.state.responses + 1 : 5
+
+    });
+    };
+
+    playAgain = () => {
+        this.getQuestions();
+        this.setState({
+            score: 0,
+            responses: 0
+        }
+        );
+
     }
     componentDidMount() {
         this.getQuestions();
@@ -29,14 +47,19 @@ class QuizJS extends Component {
         return (
             <div className="container"> {/* Check that these class names match your CSS */}
                 <div className="title">QuizJS</div>
-                {this.state.qBank.length > 0 && this.state.qBank.map(({question, answers, 
-                correct, questionID}) => 
-                <QuestionBox 
-                question={question} 
-                options={answers} 
-                key={questionID}
-                selected={answer => this.computeAnswer(answer, correct)}/>
-                )}
+                {this.state.qBank.length > 0 &&
+                    this.state.responses < 5 &&
+                    this.state.qBank.map(({ question, answers, correct, questionID }) => (
+                    <QuestionBox
+                        question={question}
+                        options={answers}
+                        key={questionID}
+                        selected={(answer) => this.computeAnswer(answer, correct)}
+                    />
+                    ))}
+
+
+                {this.state.responses === 5 ? (<Result score={this.state.score}  playAgain={this.playAgain}/>) : null}
             </div>
         );
     }
